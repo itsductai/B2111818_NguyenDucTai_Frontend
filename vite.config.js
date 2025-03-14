e40e870b74427b314f4c304434d032bc326d5704
Nguyen Duc Tai <ductaaii.2002@gmail.com>
2025-03-14 20:07:31 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()], // Thêm plugin Vue vào Vite
+  plugins: [vue()],
   server: {
     port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    },
   },
 });
